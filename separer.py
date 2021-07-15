ad6c853767a0fd883fc73b6e0ace1df2feadd1e1
Marlin <33671365+Marlin336@users.noreply.github.com>
2021-07-15 03:45:39 +0300

    Extracting sections

--- separer.py
+++ separer.py
@@ -1,9 +1,11 @@
+import os
 import headers
 from headers import *
 
 
 class Separer:
     def __init__(self, path: str):
+        self.path = path
         exe = open(path, 'rb')
         offset = 0
         self.DOS_header = DOS_header(exe.read(DOS_HEADER_LENGTH))
@@ -23,3 +25,14 @@ class Separer:
         self.Section_table = list()
         for section_num in range(self.File_header.number_of_sections):
             self.Section_table.append(Section_data(exe.read(SECTION_DATA_LENGTH)))
+            offset += SECTION_DATA_LENGTH
+        self.Section_content = list()
+        for section_num in range(self.File_header.number_of_sections):
+            self.Section_content.append(exe.read(self.Section_table[section_num].size_of_raw_data))
+
+    def extract_sections(self):
+        os.mkdir(f'{self.path}.sections')
+        os.chdir(f'{self.path}.sections')
+        for sec_num in range(len(self.Section_table)):
+            name = bytes([x for x in self.Section_table[sec_num].name if x != 0]).decode('utf-8')
+            open(name, 'wb').write(self.Section_content[sec_num])
